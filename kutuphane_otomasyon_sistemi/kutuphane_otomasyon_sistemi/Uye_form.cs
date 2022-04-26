045312ef8af8d6aab4d5f4dabfe69bdb89ba19f6
EyupEnsarKus1 <eyup@finfree.co>
2022-04-27 02:35:17 +0300

    fixed

--- kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Uye_form.cs
+++ kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Uye_form.cs
@@ -7,8 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySqlConnector;
-
+using MySql.Data.MySqlClient;
 namespace kutuphane_otomasyon_sistemi
 {
     public partial class Uye_form : Form
