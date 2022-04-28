32895ff871f8d3a0aeb3288b05b743ed9cb8d935
ZeynepKus <101632938+ZeynepKus@users.noreply.github.com>
2022-04-28 15:10:46 +0300

    eklendi

--- kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Kitap_form.Designer.cs
+++ kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Kitap_form.Designer.cs
@@ -29,11 +29,9 @@ namespace kutuphane_otomasyon_sistemi
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtKitapId = new System.Windows.Forms.TextBox();
             this.txtKitapAdı = new System.Windows.Forms.TextBox();
             this.txtYazar = new System.Windows.Forms.TextBox();
             this.txtYayınevi = new System.Windows.Forms.TextBox();
@@ -50,135 +48,124 @@ namespace kutuphane_otomasyon_sistemi
             this.txtBarkodId = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(110, 79);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "KİTAP ID";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(110, 112);
+            this.label2.Location = new System.Drawing.Point(104, 83);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.Size = new System.Drawing.Size(68, 14);
             this.label2.TabIndex = 1;
             this.label2.Text = "KİTAP ADI";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(110, 148);
+            this.label3.Location = new System.Drawing.Point(104, 123);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.Size = new System.Drawing.Size(46, 14);
             this.label3.TabIndex = 2;
             this.label3.Text = "YAZAR";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label4.Location = new System.Drawing.Point(110, 184);
+            this.label4.Location = new System.Drawing.Point(104, 159);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.Size = new System.Drawing.Size(64, 14);
             this.label4.TabIndex = 3;
             this.label4.Text = "YAYINEVİ";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // txtKitapId
-            // 
-            this.txtKitapId.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtKitapId.Location = new System.Drawing.Point(206, 72);
-            this.txtKitapId.Name = "txtKitapId";
-            this.txtKitapId.Size = new System.Drawing.Size(100, 20);
-            this.txtKitapId.TabIndex = 4;
-            // 
             // txtKitapAdı
             // 
             this.txtKitapAdı.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtKitapAdı.Location = new System.Drawing.Point(206, 105);
+            this.txtKitapAdı.Location = new System.Drawing.Point(206, 80);
             this.txtKitapAdı.Name = "txtKitapAdı";
-            this.txtKitapAdı.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapAdı.Size = new System.Drawing.Size(143, 20);
             this.txtKitapAdı.TabIndex = 5;
             // 
             // txtYazar
             // 
             this.txtYazar.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtYazar.Location = new System.Drawing.Point(206, 141);
+            this.txtYazar.Location = new System.Drawing.Point(206, 116);
             this.txtYazar.Name = "txtYazar";
-            this.txtYazar.Size = new System.Drawing.Size(100, 20);
+            this.txtYazar.Size = new System.Drawing.Size(143, 20);
             this.txtYazar.TabIndex = 6;
             // 
             // txtYayınevi
             // 
             this.txtYayınevi.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtYayınevi.Location = new System.Drawing.Point(206, 177);
+            this.txtYayınevi.Location = new System.Drawing.Point(206, 152);
             this.txtYayınevi.Name = "txtYayınevi";
-            this.txtYayınevi.Size = new System.Drawing.Size(100, 20);
+            this.txtYayınevi.Size = new System.Drawing.Size(143, 20);
             this.txtYayınevi.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label5.Location = new System.Drawing.Point(110, 220);
+            this.label5.Location = new System.Drawing.Point(104, 195);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.Size = new System.Drawing.Size(79, 14);
             this.label5.TabIndex = 8;
             this.label5.Text = "KİTAP TÜRÜ";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label6.Location = new System.Drawing.Point(108, 253);
+            this.label6.Location = new System.Drawing.Point(104, 228);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.Size = new System.Drawing.Size(89, 14);
             this.label6.TabIndex = 9;
             this.label6.Text = "SAYFA SAYISI";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label7.Location = new System.Drawing.Point(110, 285);
+            this.label7.Location = new System.Drawing.Point(104, 264);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.Size = new System.Drawing.Size(30, 14);
             this.label7.TabIndex = 10;
             this.label7.Text = "RAF";
             // 
             // txtKitapTürü
             // 
             this.txtKitapTürü.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtKitapTürü.Location = new System.Drawing.Point(206, 213);
+            this.txtKitapTürü.Location = new System.Drawing.Point(206, 188);
             this.txtKitapTürü.Name = "txtKitapTürü";
-            this.txtKitapTürü.Size = new System.Drawing.Size(100, 20);
+            this.txtKitapTürü.Size = new System.Drawing.Size(143, 20);
             this.txtKitapTürü.TabIndex = 11;
             // 
             // txtSayfaSayısı
             // 
             this.txtSayfaSayısı.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtSayfaSayısı.Location = new System.Drawing.Point(206, 246);
+            this.txtSayfaSayısı.Location = new System.Drawing.Point(206, 221);
             this.txtSayfaSayısı.Name = "txtSayfaSayısı";
-            this.txtSayfaSayısı.Size = new System.Drawing.Size(100, 20);
+            this.txtSayfaSayısı.Size = new System.Drawing.Size(143, 20);
             this.txtSayfaSayısı.TabIndex = 12;
             // 
             // txtRaf
             // 
             this.txtRaf.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtRaf.Location = new System.Drawing.Point(206, 278);
+            this.txtRaf.Location = new System.Drawing.Point(206, 257);
             this.txtRaf.Name = "txtRaf";
-            this.txtRaf.Size = new System.Drawing.Size(100, 20);
+            this.txtRaf.Size = new System.Drawing.Size(143, 20);
             this.txtRaf.TabIndex = 13;
             // 
             // txtKaydet
             // 
+            this.txtKaydet.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txtKaydet.ForeColor = System.Drawing.SystemColors.ControlText;
             this.txtKaydet.Location = new System.Drawing.Point(113, 377);
             this.txtKaydet.Name = "txtKaydet";
@@ -190,6 +177,7 @@ namespace kutuphane_otomasyon_sistemi
             // 
             // txtSil
             // 
+            this.txtSil.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txtSil.ForeColor = System.Drawing.SystemColors.ControlText;
             this.txtSil.Location = new System.Drawing.Point(261, 377);
             this.txtSil.Name = "txtSil";
@@ -200,6 +188,7 @@ namespace kutuphane_otomasyon_sistemi
             // 
             // txtİptal
             // 
+            this.txtİptal.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txtİptal.ForeColor = System.Drawing.SystemColors.ControlText;
             this.txtİptal.Location = new System.Drawing.Point(409, 377);
             this.txtİptal.Name = "txtİptal";
@@ -211,24 +200,26 @@ namespace kutuphane_otomasyon_sistemi
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label8.Location = new System.Drawing.Point(110, 325);
+            this.label8.Location = new System.Drawing.Point(104, 301);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(66, 13);
+            this.label8.Size = new System.Drawing.Size(76, 14);
             this.label8.TabIndex = 17;
             this.label8.Text = "BARKOD ID";
             // 
             // txtBarkodId
             // 
-            this.txtBarkodId.Location = new System.Drawing.Point(206, 322);
+            this.txtBarkodId.Location = new System.Drawing.Point(206, 294);
             this.txtBarkodId.Name = "txtBarkodId";
-            this.txtBarkodId.Size = new System.Drawing.Size(100, 20);
+            this.txtBarkodId.Size = new System.Drawing.Size(143, 20);
             this.txtBarkodId.TabIndex = 18;
             // 
             // Kitap_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
             this.ClientSize = new System.Drawing.Size(675, 461);
             this.Controls.Add(this.txtBarkodId);
             this.Controls.Add(this.label8);
@@ -244,11 +235,9 @@ namespace kutuphane_otomasyon_sistemi
             this.Controls.Add(this.txtYayınevi);
             this.Controls.Add(this.txtYazar);
             this.Controls.Add(this.txtKitapAdı);
-            this.Controls.Add(this.txtKitapId);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.Highlight;
             this.Name = "Kitap_form";
             this.Text = "Kitap_form";
@@ -259,12 +248,9 @@ namespace kutuphane_otomasyon_sistemi
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtKitapId;
         private System.Windows.Forms.TextBox txtKitapAdı;
         private System.Windows.Forms.TextBox txtYazar;
         private System.Windows.Forms.TextBox txtYayınevi;
