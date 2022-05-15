d50d566ac391391501add32651e57698cda96034
ZeynepKus <103748956+ZeynepKus@users.noreply.github.com>
2022-05-15 23:22:39 +0300

    veritabanına bağlantı fonksiyonu eklendi.

--- kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/UyeDatabase.cs
+++ kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/UyeDatabase.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace kutuphane_otomasyon_sistemi
 {
     class UyeDatabase
     {
+        public static MySqlConnection GetConnection()
+        {
+            string sql = "SERVER=172.21.54.3;DATABASE=foursquare;UID=foursquare;password=P16052022!t";
+            MySqlConnection con = new MySqlConnection(sql);
+            try
+            {
+                con.Open();
+
+            }
+            catch (MySqlException excep)
+            {
+                MessageBox.Show("MySqlConnection!! \n" + excep.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return con;
+        }
+
     }
 }
