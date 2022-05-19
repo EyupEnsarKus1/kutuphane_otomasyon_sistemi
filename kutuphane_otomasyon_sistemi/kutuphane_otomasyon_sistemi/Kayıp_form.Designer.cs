56d05a9e806f867347307520e7bd715969e01ce7
SudeNKK <103219352+SudeNKK@users.noreply.github.com>
2022-05-19 23:56:55 +0300

    Hakkında kısmına ufak açıklama girdim

--- kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Kayıp_form.Designer.cs
+++ kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Kayıp_form.Designer.cs
@@ -29,21 +29,36 @@ namespace kutuphane_otomasyon_sistemi
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(641, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bu Kütüphane Otomasyon Sistemi, Uludağ Üniversitesi için hazırlanmıştır. ";
             // 
             // Kayıp_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Kayıp_form";
             this.Text = "Kayıp_form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
