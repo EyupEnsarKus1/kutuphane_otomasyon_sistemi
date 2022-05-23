02f4e323bddbb00716bebd56cb2f3c84eb8bde6e
busechd <101830892+busechd@users.noreply.github.com>
2022-05-23 16:18:59 +0300

    personel kayıt formu düzenlendi

--- kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/personel_Kayıt_Form.Designer.cs
+++ kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/personel_Kayıt_Form.Designer.cs
@@ -44,83 +44,95 @@ namespace kutuphane_otomasyon_sistemi
             // Ad
             // 
             this.Ad.AutoSize = true;
-            this.Ad.Location = new System.Drawing.Point(108, 61);
+            this.Ad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Ad.Location = new System.Drawing.Point(72, 60);
             this.Ad.Name = "Ad";
-            this.Ad.Size = new System.Drawing.Size(27, 17);
+            this.Ad.Size = new System.Drawing.Size(51, 29);
             this.Ad.TabIndex = 0;
-            this.Ad.Text = "AD";
+            this.Ad.Text = "Ad:";
             // 
             // Soyd
             // 
             this.Soyd.AutoSize = true;
-            this.Soyd.Location = new System.Drawing.Point(108, 95);
+            this.Soyd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Soyd.Location = new System.Drawing.Point(72, 103);
             this.Soyd.Name = "Soyd";
-            this.Soyd.Size = new System.Drawing.Size(48, 17);
+            this.Soyd.Size = new System.Drawing.Size(93, 29);
             this.Soyd.TabIndex = 1;
-            this.Soyd.Text = "Soyad";
+            this.Soyd.Text = "Soyad:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(108, 140);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(72, 143);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.Size = new System.Drawing.Size(165, 29);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Kullanıcı Adı";
+            this.label3.Text = "Kullanıcı Adı:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(108, 181);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(72, 188);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 17);
+            this.label4.Size = new System.Drawing.Size(75, 29);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Şifre";
+            this.label4.Text = "Şifre:";
             // 
             // txtAd
             // 
-            this.txtAd.Location = new System.Drawing.Point(274, 58);
+            this.txtAd.Location = new System.Drawing.Point(258, 51);
+            this.txtAd.Multiline = true;
             this.txtAd.Name = "txtAd";
-            this.txtAd.Size = new System.Drawing.Size(100, 22);
+            this.txtAd.Size = new System.Drawing.Size(221, 38);
             this.txtAd.TabIndex = 4;
             // 
             // txtSoyad
             // 
-            this.txtSoyad.Location = new System.Drawing.Point(274, 92);
+            this.txtSoyad.Location = new System.Drawing.Point(258, 95);
+            this.txtSoyad.Multiline = true;
             this.txtSoyad.Name = "txtSoyad";
-            this.txtSoyad.Size = new System.Drawing.Size(100, 22);
+            this.txtSoyad.Size = new System.Drawing.Size(221, 37);
             this.txtSoyad.TabIndex = 5;
             // 
             // txtKadi
             // 
-            this.txtKadi.Location = new System.Drawing.Point(274, 135);
+            this.txtKadi.Location = new System.Drawing.Point(258, 135);
+            this.txtKadi.Multiline = true;
             this.txtKadi.Name = "txtKadi";
-            this.txtKadi.Size = new System.Drawing.Size(100, 22);
+            this.txtKadi.Size = new System.Drawing.Size(221, 37);
             this.txtKadi.TabIndex = 6;
             // 
             // txtSifre
             // 
-            this.txtSifre.Location = new System.Drawing.Point(274, 178);
+            this.txtSifre.Location = new System.Drawing.Point(258, 178);
+            this.txtSifre.Multiline = true;
             this.txtSifre.Name = "txtSifre";
-            this.txtSifre.Size = new System.Drawing.Size(100, 22);
+            this.txtSifre.Size = new System.Drawing.Size(221, 39);
             this.txtSifre.TabIndex = 7;
             // 
             // btnEkle
             // 
-            this.btnEkle.Location = new System.Drawing.Point(196, 231);
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(80)))), ((int)(((byte)(61)))));
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.ForeColor = System.Drawing.Color.Black;
+            this.btnEkle.Location = new System.Drawing.Point(77, 238);
             this.btnEkle.Name = "btnEkle";
-            this.btnEkle.Size = new System.Drawing.Size(254, 29);
+            this.btnEkle.Size = new System.Drawing.Size(402, 64);
             this.btnEkle.TabIndex = 8;
             this.btnEkle.Text = "Ekle";
-            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.UseVisualStyleBackColor = false;
             this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
             // 
             // lblHead
             // 
             this.lblHead.AutoSize = true;
-            this.lblHead.Location = new System.Drawing.Point(271, 19);
+            this.lblHead.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHead.Location = new System.Drawing.Point(251, 9);
             this.lblHead.Name = "lblHead";
-            this.lblHead.Size = new System.Drawing.Size(91, 17);
+            this.lblHead.Size = new System.Drawing.Size(236, 39);
             this.lblHead.TabIndex = 9;
             this.lblHead.Text = "Kullanıcı Ekle";
             // 
@@ -128,6 +140,7 @@ namespace kutuphane_otomasyon_sistemi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(216)))), ((int)(((byte)(254)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblHead);
             this.Controls.Add(this.btnEkle);
