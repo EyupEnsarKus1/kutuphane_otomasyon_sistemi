92009192b13ca3f15dcab10800e4b9f77e9681cf
ZeynepKus <103748956+ZeynepKus@users.noreply.github.com>
2022-05-24 10:46:54 +0300

    üye kayıt form tasarımı düzenlendi

--- kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Uye_Kayıt_Form.Designer.cs
+++ kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Uye_Kayıt_Form.Designer.cs
@@ -48,74 +48,80 @@ namespace kutuphane_otomasyon_sistemi
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.Location = new System.Drawing.Point(148, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.Size = new System.Drawing.Size(135, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "ÜYE NUMARA:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.Location = new System.Drawing.Point(148, 108);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.Size = new System.Drawing.Size(46, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "ADI:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.Location = new System.Drawing.Point(148, 152);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.Size = new System.Drawing.Size(83, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "SOYADI:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label4.Location = new System.Drawing.Point(148, 196);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.Size = new System.Drawing.Size(97, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "CİNSİYET:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label5.Location = new System.Drawing.Point(148, 244);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.Size = new System.Drawing.Size(99, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "TELEFON:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label6.Location = new System.Drawing.Point(148, 291);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.Size = new System.Drawing.Size(70, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "EMAİL:";
             // 
             // txtUyeNumara
             // 
-            this.txtUyeNumara.Location = new System.Drawing.Point(262, 54);
+            this.txtUyeNumara.Location = new System.Drawing.Point(289, 60);
             this.txtUyeNumara.Name = "txtUyeNumara";
             this.txtUyeNumara.Size = new System.Drawing.Size(100, 22);
             this.txtUyeNumara.TabIndex = 6;
             // 
             // txtAd
             // 
-            this.txtAd.Location = new System.Drawing.Point(262, 102);
+            this.txtAd.Location = new System.Drawing.Point(289, 108);
             this.txtAd.Name = "txtAd";
             this.txtAd.Size = new System.Drawing.Size(100, 22);
             this.txtAd.TabIndex = 7;
             // 
             // txtSoyad
             // 
-            this.txtSoyad.Location = new System.Drawing.Point(262, 146);
+            this.txtSoyad.Location = new System.Drawing.Point(289, 152);
             this.txtSoyad.Name = "txtSoyad";
             this.txtSoyad.Size = new System.Drawing.Size(100, 22);
             this.txtSoyad.TabIndex = 8;
@@ -126,33 +132,35 @@ namespace kutuphane_otomasyon_sistemi
             this.comboCinsiyet.Items.AddRange(new object[] {
             "KADIN",
             "ERKEK"});
-            this.comboCinsiyet.Location = new System.Drawing.Point(262, 188);
+            this.comboCinsiyet.Location = new System.Drawing.Point(289, 192);
             this.comboCinsiyet.Name = "comboCinsiyet";
             this.comboCinsiyet.Size = new System.Drawing.Size(121, 24);
             this.comboCinsiyet.TabIndex = 9;
             // 
             // txtTelefon
             // 
-            this.txtTelefon.Location = new System.Drawing.Point(262, 238);
+            this.txtTelefon.Location = new System.Drawing.Point(289, 242);
             this.txtTelefon.Name = "txtTelefon";
             this.txtTelefon.Size = new System.Drawing.Size(100, 22);
             this.txtTelefon.TabIndex = 10;
             // 
             // txtEMail
             // 
-            this.txtEMail.Location = new System.Drawing.Point(262, 285);
+            this.txtEMail.Location = new System.Drawing.Point(289, 289);
             this.txtEMail.Name = "txtEMail";
             this.txtEMail.Size = new System.Drawing.Size(100, 22);
             this.txtEMail.TabIndex = 11;
             // 
             // btnEkle
             // 
+            this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(80)))), ((int)(((byte)(61)))));
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnEkle.Location = new System.Drawing.Point(200, 356);
             this.btnEkle.Name = "btnEkle";
             this.btnEkle.Size = new System.Drawing.Size(115, 52);
             this.btnEkle.TabIndex = 12;
             this.btnEkle.Text = "Ekle";
-            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.UseVisualStyleBackColor = false;
             this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
             // 
             // lblHead
@@ -160,7 +168,7 @@ namespace kutuphane_otomasyon_sistemi
             this.lblHead.AutoSize = true;
             this.lblHead.Location = new System.Drawing.Point(262, 13);
             this.lblHead.Name = "lblHead";
-            this.lblHead.Size = new System.Drawing.Size(35, 17);
+            this.lblHead.Size = new System.Drawing.Size(34, 16);
             this.lblHead.TabIndex = 13;
             this.lblHead.Text = "Ekle";
             // 
@@ -168,6 +176,7 @@ namespace kutuphane_otomasyon_sistemi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(216)))), ((int)(((byte)(254)))));
             this.ClientSize = new System.Drawing.Size(690, 671);
             this.Controls.Add(this.lblHead);
             this.Controls.Add(this.btnEkle);
