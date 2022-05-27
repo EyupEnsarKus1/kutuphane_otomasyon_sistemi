2830e41b86007b633b4d8ecbbe036c9feb5349af
IremOzkose <ozkoseirem13@gmail.com>
2022-05-27 21:25:56 +0300

    üye kayıt form tasarımı düzenlendi

--- kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Uye_Kayıt_Form.Designer.cs
+++ kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/Uye_Kayıt_Form.Designer.cs
@@ -48,82 +48,94 @@ namespace kutuphane_otomasyon_sistemi
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(148, 60);
+            this.label1.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(118, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.Size = new System.Drawing.Size(118, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "ÜYE NUMARA:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(148, 108);
+            this.label2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(118, 104);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.Size = new System.Drawing.Size(41, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "ADI:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(148, 152);
+            this.label3.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(118, 140);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.Size = new System.Drawing.Size(70, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "SOYADI:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(148, 196);
+            this.label4.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(118, 174);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.Size = new System.Drawing.Size(82, 19);
             this.label4.TabIndex = 3;
             this.label4.Text = "CİNSİYET:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(148, 244);
+            this.label5.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(118, 214);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(99, 20);
+            this.label5.Size = new System.Drawing.Size(84, 19);
             this.label5.TabIndex = 4;
             this.label5.Text = "TELEFON:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(148, 291);
+            this.label6.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(118, 252);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(70, 20);
+            this.label6.Size = new System.Drawing.Size(61, 19);
             this.label6.TabIndex = 5;
             this.label6.Text = "EMAİL:";
             // 
             // txtUyeNumara
             // 
-            this.txtUyeNumara.Location = new System.Drawing.Point(289, 60);
+            this.txtUyeNumara.Location = new System.Drawing.Point(236, 65);
+            this.txtUyeNumara.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUyeNumara.Multiline = true;
             this.txtUyeNumara.Name = "txtUyeNumara";
-            this.txtUyeNumara.Size = new System.Drawing.Size(100, 22);
+            this.txtUyeNumara.Size = new System.Drawing.Size(115, 21);
             this.txtUyeNumara.TabIndex = 6;
             // 
             // txtAd
             // 
-            this.txtAd.Location = new System.Drawing.Point(289, 108);
+            this.txtAd.Location = new System.Drawing.Point(236, 104);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAd.Multiline = true;
             this.txtAd.Name = "txtAd";
-            this.txtAd.Size = new System.Drawing.Size(100, 22);
+            this.txtAd.Size = new System.Drawing.Size(115, 21);
             this.txtAd.TabIndex = 7;
             // 
             // txtSoyad
             // 
-            this.txtSoyad.Location = new System.Drawing.Point(289, 152);
+            this.txtSoyad.Location = new System.Drawing.Point(236, 140);
+            this.txtSoyad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSoyad.Multiline = true;
             this.txtSoyad.Name = "txtSoyad";
-            this.txtSoyad.Size = new System.Drawing.Size(100, 22);
+            this.txtSoyad.Size = new System.Drawing.Size(115, 21);
             this.txtSoyad.TabIndex = 8;
             // 
             // comboCinsiyet
@@ -132,32 +144,39 @@ namespace kutuphane_otomasyon_sistemi
             this.comboCinsiyet.Items.AddRange(new object[] {
             "KADIN",
             "ERKEK"});
-            this.comboCinsiyet.Location = new System.Drawing.Point(289, 192);
+            this.comboCinsiyet.Location = new System.Drawing.Point(236, 172);
+            this.comboCinsiyet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboCinsiyet.Name = "comboCinsiyet";
-            this.comboCinsiyet.Size = new System.Drawing.Size(121, 24);
+            this.comboCinsiyet.Size = new System.Drawing.Size(115, 21);
             this.comboCinsiyet.TabIndex = 9;
             // 
             // txtTelefon
             // 
-            this.txtTelefon.Location = new System.Drawing.Point(289, 242);
+            this.txtTelefon.Location = new System.Drawing.Point(236, 213);
+            this.txtTelefon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTelefon.Multiline = true;
             this.txtTelefon.Name = "txtTelefon";
-            this.txtTelefon.Size = new System.Drawing.Size(100, 22);
+            this.txtTelefon.Size = new System.Drawing.Size(115, 21);
             this.txtTelefon.TabIndex = 10;
             // 
             // txtEMail
             // 
-            this.txtEMail.Location = new System.Drawing.Point(289, 289);
+            this.txtEMail.Location = new System.Drawing.Point(236, 251);
+            this.txtEMail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtEMail.Multiline = true;
             this.txtEMail.Name = "txtEMail";
-            this.txtEMail.Size = new System.Drawing.Size(100, 22);
+            this.txtEMail.Size = new System.Drawing.Size(115, 21);
             this.txtEMail.TabIndex = 11;
             // 
             // btnEkle
             // 
             this.btnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(80)))), ((int)(((byte)(61)))));
-            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnEkle.Location = new System.Drawing.Point(200, 356);
+            this.btnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEkle.Font = new System.Drawing.Font("Constantia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(184, 305);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEkle.Name = "btnEkle";
-            this.btnEkle.Size = new System.Drawing.Size(115, 52);
+            this.btnEkle.Size = new System.Drawing.Size(120, 30);
             this.btnEkle.TabIndex = 12;
             this.btnEkle.Text = "Ekle";
             this.btnEkle.UseVisualStyleBackColor = false;
@@ -166,18 +185,20 @@ namespace kutuphane_otomasyon_sistemi
             // lblHead
             // 
             this.lblHead.AutoSize = true;
-            this.lblHead.Location = new System.Drawing.Point(262, 13);
+            this.lblHead.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHead.Location = new System.Drawing.Point(211, 21);
+            this.lblHead.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblHead.Name = "lblHead";
-            this.lblHead.Size = new System.Drawing.Size(34, 16);
+            this.lblHead.Size = new System.Drawing.Size(43, 19);
             this.lblHead.TabIndex = 13;
             this.lblHead.Text = "Ekle";
             // 
             // Uye_Kayıt_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(216)))), ((int)(((byte)(254)))));
-            this.ClientSize = new System.Drawing.Size(690, 671);
+            this.ClientSize = new System.Drawing.Size(518, 545);
             this.Controls.Add(this.lblHead);
             this.Controls.Add(this.btnEkle);
             this.Controls.Add(this.txtEMail);
@@ -192,6 +213,7 @@ namespace kutuphane_otomasyon_sistemi
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Uye_Kayıt_Form";
             this.Text = "Uye_Kayıt_Form";
             this.Load += new System.EventHandler(this.Uye_Kayıt_Form_Load);
