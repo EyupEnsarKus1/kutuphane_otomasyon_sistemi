0625ab3a299fda765bc2a38a51060712622e297b
ZeynepKus <103748956+ZeynepKus@users.noreply.github.com>
2022-05-29 22:33:47 +0300

    Ödünç alma form sayfası düzenlendi

--- kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/odunc_Form.Designer.cs
+++ kutuphane_otomasyon_sistemi/kutuphane_otomasyon_sistemi/odunc_Form.Designer.cs
@@ -127,36 +127,40 @@ namespace kutuphane_otomasyon_sistemi
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(228, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.Size = new System.Drawing.Size(130, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Emanet Kitap Listesi ";
             // 
             // txtAra
             // 
             this.txtAra.Location = new System.Drawing.Point(98, 517);
+            this.txtAra.Multiline = true;
             this.txtAra.Name = "txtAra";
-            this.txtAra.Size = new System.Drawing.Size(100, 22);
+            this.txtAra.Size = new System.Drawing.Size(152, 25);
             this.txtAra.TabIndex = 2;
             this.txtAra.TextChanged += new System.EventHandler(this.txtAra_TextChanged);
             // 
             // btnTeslimAl
             // 
+            this.btnTeslimAl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(80)))), ((int)(((byte)(61)))));
+            this.btnTeslimAl.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnTeslimAl.Location = new System.Drawing.Point(532, 451);
             this.btnTeslimAl.Name = "btnTeslimAl";
             this.btnTeslimAl.Size = new System.Drawing.Size(271, 44);
             this.btnTeslimAl.TabIndex = 3;
             this.btnTeslimAl.Text = "Teslim Al";
-            this.btnTeslimAl.UseVisualStyleBackColor = true;
+            this.btnTeslimAl.UseVisualStyleBackColor = false;
             this.btnTeslimAl.Click += new System.EventHandler(this.btnTeslimAl_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.Location = new System.Drawing.Point(53, 465);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(195, 17);
+            this.label2.Size = new System.Drawing.Size(274, 24);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Uye Numarasına Göre Filtrele";
+            this.label2.Text = "Üye Numarasına Göre Filtrele";
             // 
             // odunc_Form
             // 
